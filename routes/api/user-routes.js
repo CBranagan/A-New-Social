848c7c85ef6998656e8aa32957eda203e7ed2c56
Chad Branagan <chad.branagan@gmail.com>
2022-01-29 11:53:15 -0800

    add and delete friend routes working

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,12 +6,17 @@ const {
   getUserById,
   updateUser,
   deleteUser,
+  addFriend,
+  deleteFriend,
 } = require("../../contollers/user-controller");
 
 // /api/user
 router.route("/").get(getAllUsers).post(createUser);
 
 // /api/user/:id
-router.route("./:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/user/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
