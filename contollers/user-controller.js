83d92538b944fc23580748eebe0c1d5f7c70c933
Chad Branagan <chad.branagan@gmail.com>
2022-01-29 13:17:39 -0800

    code cleanup

--- contollers/user-controller.js
+++ contollers/user-controller.js
@@ -4,10 +4,7 @@ const userController = {
   // get all users
   getAllUsers(req, res) {
     User.find({})
-      //   .populate({
-      //     path: "Thought",
-      //     select: "-__v",
-      //   })
+
       .select("-__v")
       .sort({ _id: -1 })
       .then((dbUserData) => res.json(dbUserData))
@@ -19,10 +16,7 @@ const userController = {
   // get one user by id
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
-      //   .populate({
-      //     path: "thought",
-      //     select: "-__v",
-      //   })
+
       .select("-__v")
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
