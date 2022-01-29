7b564b264a7b1c2e63d99135f68845a33a1ece8b
Chad Branagan <chad.branagan@gmail.com>
2022-01-29 10:21:01 -0800

    Thought and reaction Schemas built

---
+++ Models/Thought.js
@@ -0,0 +1,60 @@
+import { Schema, model, Types } from "mongoose";
+import dateFormat from "../utils/dateFormat";
+
+const ReactionSchema = new Schema({
+  reactionID: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: [true, "You must eneter a reaction!"],
+    min: 1,
+    max: 280,
+  },
+  userName: {
+    type: String,
+    required: [true, "You must enter a User Name!"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+});
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: [true, "You must enter a thought!"],
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    userName: {
+      type: String,
+      required: [true, "Please enter your User Name"],
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
